f9eddae7682fc603c4ec10e8b1db1fa9ecd72cc2
KYW <qodna25@gmail.com>
2023-06-15 10:43:42 +0900

    tit : simple_pm2.js desc : 잘못된 요청으로 서버 강제 종료시키기

---
+++ nodejs/4-3_simple_pm2.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const PORT =2020
+
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type','application/json ; charset=uft-8')
+
+    const obj ={
+        "이름":"김김"
+    }
+
+    res.end(JSON.stringify(obj))
+})
+
+// 5초 뒤에 서버가 종료된다 {Z 는 올바른 JSON 형태가 아니기 때문이다.
+setTimeout(()=>{
+JSON.parse("{Z")
+},5000)
+
+server.listen(PORT,()=>{
+    console.log(`${PORT} 서버 오픈`)
+})
