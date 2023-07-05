1d5e2b4ba46b4c7736a03748a66d8ab0dc9be5d7
KYW <qodna25@gmail.com>
2023-07-05 17:47:03 +0900

    임시커밋

--- note/p346_MongooseCRUD/CRUD_read.js
+++ note/p346_MongooseCRUD/CRUD_read.js
@@ -2,7 +2,12 @@ let Schema_ori = require('./CRUD_ori.js');
 
 const main = async () => {
   const CRUD_C = new Schema_ori(_data);
-  const t = await CRUD_C.save();
+  const t = await CRUD_C.find(
+    {
+      title: { $eq: '홍길동' },
+    },
+    { _id: 0 }
+  ).lean();
   console.log(t);
 };
 
